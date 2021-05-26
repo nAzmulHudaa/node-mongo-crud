4fc9165779c33d425530becc7f47eb2c604bde40
Md Nazmul Huda Rimon <nhr.developer@gmail.com>
2021-05-26 14:18:20 +0600

    database connected

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://nazmulhuda:62968512ab@cluster0.0gacq.mongodb.net/organicdb?retryWrites=true&w=majority";
 
 const app = express();
+
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("organicdb").collection("collection");
+  console.log('db connected')
+});
 
 app.use(cors());
 app.use(bodyParser.json());
